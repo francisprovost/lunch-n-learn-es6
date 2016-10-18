d7a7fb903021f5291dfe6d9bf6ec320df3a77dee
Francis Provost <fprovost@solutionsmedias360.com>
2016-10-17 22:43:29 -0400

    Setting up basics (yarn/npm, gulp and babel)

---
+++ dist/all.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function helloWorld() {
+    var name = 'Morty';
+    console.log('Hello ' + name);
+}
+//# sourceMappingURL=all.js.map
---
+++ src/index.js
@@ -0,0 +1,3 @@
+let helloWorld = name => {
+    console.log(`Hello ${name}`);
+}
